a91b8697b80bc3039358db280680066219d45d83
alfredohjr <alfredojrgasper@gmail.com>
2022-01-23 12:40:45 -0300

    update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
+    assetPrefix: isProd ? 'https://cdn.mydomain.com' : '',
     async headers() {
         return [
             {
